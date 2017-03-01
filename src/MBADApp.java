d044ad4b1c0f1d0739ef383f5c8eec54a1bf4d1b
afashokova <afashokova95@gmail.com>
2017-03-01 17:15:32 +0300

    44

--- src/MBADApp.java
+++ src/MBADApp.java
@@ -2,13 +2,26 @@
  * Created by afashokova on 02.02.2017.
  */
 
+import com.google.common.base.Function;
+import edu.uci.ics.jung.algorithms.layout.CircleLayout;
+import edu.uci.ics.jung.algorithms.layout.Layout;
+import edu.uci.ics.jung.visualization.BasicVisualizationServer;
+import edu.uci.ics.jung.visualization.decorators.ToStringLabeller;
+import edu.uci.ics.jung.visualization.renderers.Renderer;
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Paint;
+import java.awt.Stroke;
+import javax.swing.JFrame;
+import org.apache.commons.collections15.Transformer;
+
+
+
 public class MBADApp {
     public static void main(String args[]) throws Exception {
-        ReadData readData = new ReadData();
-        try {
-            readData.readProxOut();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        graf g = new graf();
+        g.NearestPath();
+
     }
 }
---
+++ src/graf.java
@@ -0,0 +1,52 @@
+import edu.uci.ics.jung.algorithms.shortestpath.DijkstraDistance;
+import edu.uci.ics.jung.algorithms.shortestpath.DijkstraShortestPath;
+import edu.uci.ics.jung.graph.SparseGraph;
+import edu.uci.ics.jung.graph.Graph;
+import edu.uci.ics.jung.graph.util.EdgeType;
+
+import java.util.List;
+
+
+/**
+ * Created by afashokova on 27.02.2017.
+ */
+public class graf {
+   Graph<Integer, String> graph;
+     public graf() {
+        graph = new SparseGraph<Integer, String>();
+        for (int i = 0; i<21;i++)
+            graph.addVertex(i);
+        //1этаж
+        graph.addEdge("1-1,1-2",0,1, EdgeType.UNDIRECTED);
+        graph.addEdge("1-1,1-3",0,2,EdgeType.UNDIRECTED);
+        graph.addEdge("1-1,1-4",0,3,EdgeType.UNDIRECTED);
+        graph.addEdge("1-1,1-5",0,4,EdgeType.UNDIRECTED);
+        graph.addEdge("1-1,1-6",0,5,EdgeType.UNDIRECTED);
+        graph.addEdge("1-1,1-7",0,6,EdgeType.UNDIRECTED);
+        graph.addEdge("1-1,1-8",0,7,EdgeType.UNDIRECTED);
+        graph.addEdge("1-4,2-4",3,11,EdgeType.UNDIRECTED);
+        graph.addEdge("2,1-2",8,9,EdgeType.UNDIRECTED);
+        graph.addEdge("2,1-4",8,11,EdgeType.UNDIRECTED);
+        graph.addEdge("2,1-6",8,13,EdgeType.UNDIRECTED);
+        graph.addEdge("2,1-7",8,14,EdgeType.UNDIRECTED);
+        graph.addEdge("2,2-5",9,12,EdgeType.UNDIRECTED);
+        graph.addEdge("2,3-7",10,14,EdgeType.UNDIRECTED);
+        graph.addEdge("2,4-7",11,14,EdgeType.UNDIRECTED);
+        graph.addEdge("2-4,3-4",11,18,EdgeType.UNDIRECTED);
+        graph.addEdge("2,6-7",13,14,EdgeType.UNDIRECTED);
+        graph.addEdge("3,1-2",15,16,EdgeType.UNDIRECTED);
+        graph.addEdge("3,1-3",15,17,EdgeType.UNDIRECTED);
+        graph.addEdge("3,1-4",15,18,EdgeType.UNDIRECTED);
+        graph.addEdge("3,1-6",15,20,EdgeType.UNDIRECTED);
+        graph.addEdge("3,2-5",16,19,EdgeType.UNDIRECTED);
+        graph.addEdge("3,3-6",17,20,EdgeType.UNDIRECTED);
+
+}
+      public void NearestPath(){
+         DijkstraShortestPath<Integer,String> alg = new DijkstraShortestPath(graph);
+         List<String> l = alg.getPath(20,0);
+         System.out.println("Кратчайший путь "+ l);
+         DijkstraDistance<Integer,String> alg2 = new DijkstraDistance<Integer, String>(graph);
+         Number l2 = alg2.getDistance(0,20);
+         System.out.println("расстояние " +l2 );
+      }}
