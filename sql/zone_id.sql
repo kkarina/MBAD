6a062a1f43c6be29ad0a9abdc2caca2aad6dbe45
afashokova <afashokova95@gmail.com>
2017-03-07 13:31:58 +0300

    44

---
+++ sql/zone_id.sql
@@ -0,0 +1,8 @@
+create table mbad.zone_id
+(id integer, zone char(15));
+
+create table mbad.proxout
+(timestamp char (50), employee_id char (30), zone char (20));
+
+create table mbad.employee
+(id char(30), depertment_id integer, office integer);
